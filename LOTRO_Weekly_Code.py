c7e533e13fec3511394ae3fe09a439f3d06ba55b
Git-Forked <66106762+Git-Forked@users.noreply.github.com>
2024-08-07 02:45:08 +0000

    Update LOTRO_Weekly_Code.py

--- LOTRO_Weekly_Code.py
+++ LOTRO_Weekly_Code.py
@@ -5,15 +5,15 @@ import urllib3
 urllib3.disable_warnings()
 urllib3.util.ssl_.DEFAULT_CIPHERS += 'HIGH:!DH:!aNULL'
 http = urllib3.PoolManager(cert_reqs='CERT_NONE')
-r = http.request('GET', 'https://forums.lotro.com/index.php?forums/sales-and-promotions.8/&last_days=7&order=last_post_date&direction=dec')
+
+r = http.request('GET', 'https://forums.lotro.com/index.php?forums/sales-and-promotions.8/index.rss')
 
 def str_between(string, start, end):
     return (string.split(start))[1].split(end)[0]
 
-result = str_between(str(r.data), 'data-preview-url="', '">')
-
-r = http.request('GET', 'https://forums.lotro.com' + result)
-
-result = str_between(str(r.data), 'Coupon Code: ', '<br />')
+try:
+    result = str_between(str(r.data), 'Coupon Code: ', '<br />')
+except:
+    print("ERROR: Unable to retrieve Coupon Code.")
 
 print(result)
